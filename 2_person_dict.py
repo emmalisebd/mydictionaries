e8588dfcaa39cfcfdaff7a02a31bb136a5db80c7
Emmalise Dunnavan <emmalise_dunnavan1@baylor.edu>
2023-01-30 15:45:15 -0600

    update

--- 2_person_dict.py
+++ 2_person_dict.py
@@ -5,3 +5,21 @@ person["age"] = 51
 person["spouse"] = "Edna"
 person["children"] = ["Ralph", "Betty", "Joey"]
 person["pets"] = {"dog": "Fido", "cat": "Sox"}
+
+print(person)
+
+# print out name of the second child
+name = 'Betty'
+
+if name in person:
+    print(person[name])
+
+
+# print out name of cat
+
+
+# iterate through all children and print out each child
+
+
+# print out the pets in this format:
+# type of pet: dog name of pet: Fido
